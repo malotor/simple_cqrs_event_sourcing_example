a0922951de4b6c34bc12e7a8894a3be03e8f4a84
Manel <mlopezt@prosodie.com>
2018-05-29 17:36:09 +0200

    Move files

--- boot.rb
+++ boot.rb
@@ -14,18 +14,20 @@ require 'arkency/command_bus'
 require 'arkency/command_bus/alias'
 
 require_relative './app/model/employee/employee_events'
-require_relative './app/model/employee/employee_event_subscribers'
+require_relative './app/event_subscribers/employee_event_subscribers'
 require_relative './app/model/employee/employee'
-require_relative './app/model/employee/employee_repository'
+
 require_relative './app/model/employee_view'
+
+require_relative './app/services/persistence/employee_repository'
 #
 require_relative './lib/service_provider'
 
 
-require_relative './lib/command_bus/commands'
-require_relative './lib/command_bus/command_handlers'
-require_relative './lib/command_bus/querys'
-require_relative './lib/command_bus/query_handlers'
+require_relative './app/command_bus/commands'
+require_relative './app/command_bus/command_handlers'
+require_relative './app/command_bus/querys'
+require_relative './app/command_bus/query_handlers'
 
 Dir["#{File.dirname(__FILE__)}/app/services/**/*.rb"].each {|file| require file }
 
